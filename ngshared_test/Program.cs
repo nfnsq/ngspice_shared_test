640de43d3e1d8a3dcf328e5e95734c0a88bf81fe
nfnsq <tatyias95@gmail.com>
2017-06-12 12:34:32 +0700

    пример схемы для расчета s-параметров

--- ngshared_test/Program.cs
+++ ngshared_test/Program.cs
@@ -5,72 +5,19 @@ using System.Threading;
 using System.Numerics;
 using System.Globalization;
 using System.Windows;
+using System.Text.RegularExpressions;
 
 namespace ngshared_test
 {
-    public class Init
-    {
-        private static int cbSendChar(IntPtr param0, int param1, IntPtr param2)
-        {
-            string message = Marshal.PtrToStringAnsi(param0);
-
-            Console.WriteLine("lib {0}: {1}", param1, message);
-            return 1;
-        }
-        private static int cbSendStat(IntPtr param0, int param1, IntPtr param2)
-        {
-            string stat = Marshal.PtrToStringAnsi(param0);
-
-            Console.WriteLine("lib {0}: {1}", param1, stat);
-            return 1;
-        }
-        private static int cbControlledExit(int param0, [MarshalAs(UnmanagedType.I1)] bool param1, [MarshalAs(UnmanagedType.I1)] bool param2, int param3, IntPtr param4)
-        {
-            Console.WriteLine("contolled exit");
-            return param0;
-        }
-        private static int cbSendData(IntPtr param0, int param1, int param2, IntPtr param3)
-        {
-            return 1;
-        }
-        private static int cbSendInitData(IntPtr param0, int param1, IntPtr param2)
-        {
-            return 1;
-        }
-        private static int cbBGThreadRunnig([MarshalAs(UnmanagedType.I1)] bool param0, int param1, IntPtr param2)
-        {
-            return 1;
-        }
-
-
-        public Init()
-        {
-            SendChar sc = new SendChar(cbSendChar);
-            SendStat ss = new SendStat(cbSendStat);
-            ControlledExit ce = new ControlledExit(cbControlledExit);
-            SendData sd = new SendData(cbSendData);
-            SendInitData sid = new SendInitData(cbSendInitData);
-            BGThreadRunning bgtrun = new BGThreadRunning(cbBGThreadRunnig);
-
-            IntPtr caller = Marshal.AllocHGlobal(Marshal.SizeOf(this));
-            Marshal.StructureToPtr(this, caller, false);
-
-            NativeMethods.ngSpice_Init(
-                Marshal.GetFunctionPointerForDelegate(sc),
-                Marshal.GetFunctionPointerForDelegate(ss),
-                Marshal.GetFunctionPointerForDelegate(ce),
-                Marshal.GetFunctionPointerForDelegate(sd),
-                Marshal.GetFunctionPointerForDelegate(sid),
-                Marshal.GetFunctionPointerForDelegate(bgtrun), 
-                caller);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            //ControlledExit ce = new ControlledExit(cbControlledExit);
-            //NativeMethods.ngSpice_Init(null, null, ce, null, null, null, Marshal.GetIUnknownForObject(this));
+            string str = "1.000000e-02";
+            double x = double.Parse(str, CultureInfo.InvariantCulture);
+            Console.WriteLine(str + "\n" + x);
+            Console.ReadLine();
+            /*
             SendChar sc = new SendChar(cbSendChar);
             SendStat ss = new SendStat(cbSendStat);
             ControlledExit ce = new ControlledExit(cbControlledExit);
@@ -90,33 +37,57 @@ namespace ngshared_test
                 Marshal.GetFunctionPointerForDelegate(sid),
                 Marshal.GetFunctionPointerForDelegate(bgtrun),
                 caller);
-            //IntPtr command = Marshal.GetIUnknownForObject("bg_run");
-            //NativeMethods.ngSpice_Command(command);
-            //ngSpice_Command("source res_array.cir");
-            //ngSpice_Command("run");
+            //NativeMethods.ngSpice_Command("bg_run");
+            NativeMethods.ngSpice_Command("source ../../../test.cir");
+            NativeMethods.ngSpice_Command("run");
+            NativeMethods.ngSpice_Command("ac dec 10 0.01 100");
+            NativeMethods.ngSpice_Command("print vdb(2)");
 
             if (NativeMethods.ngSpice_running())
-         {
-             Console.WriteLine("true");
-         }
-         else
-         {
-             Console.WriteLine("false");
-         }
-         Console.ReadLine();
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+            var re = new Regex("stdout ");
+            _text = re.Replace(_text, "");
+            int index = _text.IndexOf("\t");
+            _text = _text.Substring(index - 1);
+            char delimiter = '\t';
+            string[] substrings = _text.Split(delimiter);
+            for (int i = 0; i < substrings.Length - 3; i += 3)
+            {
+                double x = double.Parse(substrings[i + 1]);
+                double y = double.Parse(substrings[i + 2]);
+
+            }
+            Console.ReadLine();
         }
+        private static string _text;
+        private static bool _write = false;
         private static int cbSendChar(IntPtr param0, int param1, IntPtr param2)
         {
             string message = Marshal.PtrToStringAnsi(param0);
+            if (Regex.IsMatch(message, "AC Analysis"))
+            {
+                _write = true;
+            }
+            if (_write)
+            {
 
-            Console.WriteLine("lib {0}: {1}", param1, message);
+                _text += message;
+            }
+
+            //Console.WriteLine("lib {0}: {1}", param1, message);
             return 1;
         }
         private static int cbSendStat(IntPtr param0, int param1, IntPtr param2)
         {
             string stat = Marshal.PtrToStringAnsi(param0);
 
-            Console.WriteLine("lib {0}: {1}", param1, stat);
+            //Console.WriteLine("lib {0}: {1}", param1, stat);
             return 1;
         }
         private static int cbControlledExit(int param0, [MarshalAs(UnmanagedType.I1)] bool param1, [MarshalAs(UnmanagedType.I1)] bool param2, int param3, IntPtr param4)
@@ -136,6 +107,7 @@ namespace ngshared_test
         {
             return 1;
         }
-
+        */
+        }
     }
 }
